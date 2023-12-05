2d7ee1c11f0afcf621697f763d5d33dc19ef55a2
Eric N <eric.nyh@icloud.com>
2023-12-05 16:28:25 +0000

    Base app is complete. Read functionality is complete.

--- index.js
+++ index.js
@@ -1,20 +1,61 @@
-const express = require('express');
-const hbs = require('hbs');
-const wax = require('wax-on');
+// mock data
+let staff = [
+    {
+        "id": 1,
+        "name": "Keong",
+        "branch": "Katong",
+        "gender": "M",
+        "services": "Foot Massage, Head and Shoulders, Back Massage"
+    },
+    {
+        "id": 2,
+        "name": "Leon",
+        "branch": "Siglap",
+        "gender": "M",
+        "services": "Foot Massage, Body Massage"
+    },
+    {
+        "id": 3,
+        "name": "Xiaofang",
+        "branch": "Upper Changi",
+        "gender": "F",
+        "services": "Foot Massage, Head and Shoulders, Back Massage, Body Massage"
+    },
+    {
+        "id": 4,
+        "name": "Brandon",
+        "branch": "East Coast",
+        "gender": "M",
+        "services": "Foot Massage, Back Massage"
+    }
+]
 
+const express = require('express'); // routing
+const hbs = require('hbs'); // templating
+const wax = require('wax-on'); // extending templates
+const helpers = require('handlebars-helpers')({ handlebars: hbs.handlebars}); // interating through arrays in templates and other fancy stuff
 const app = express();
 
+// set up default render engine
 app.set('view engine', 'hbs');
-app.use(express.urlencoded({
-    extended: true // use basic forms not advanced forms
-}))
-// TODO: Add in the code to allow Express to access the form (see lab 6 step 1)
 
+// set up path for statics files like style.css
+app.use(express.static('public'))
+
+// set up wax-on for hbs extensions, so common elements can be reused
 wax.on(hbs.handlebars);
-wax.setLayoutPath('./views/layouts')
+wax.setLayoutPath("./views/layouts");
 
-app.get("/", function(req,res){
-  res.render("index")
-})
 
-app.listen(3000,()=>console.log("Server started..."))
+//use extended = true for fancier form encoding
+app.use(express.urlencoded({ "extended": true }))
+
+
+// Set up routes
+app.get("/", function (req, res) { res.render("index", {"staff": staff}) })
+app.get("/newstaff", function (req, res) { res.render("newstaff") })
+app.get("/editstaff", function (req, res) { res.render("editstaff") })
+app.get("/deletestaff", function (req, res) { res.render("deletestaff") })
+
+// start server
+app.listen(3000, () => console.log("Server started..."))
